27a13a082aac8d9377cb364c2f1b2eb94b0ff75f
Hamed <hamed@Hameds-iMac.local>
2019-01-31 11:29:28 +0200

    Adding the properties of statusbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
@@ -19,6 +18,12 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      this.statusBar.styleLightContent();
+      // the options of status bar
+      this.statusBar.overlaysWebView(false);
+      // set the color of background status bar
+      this.statusBar.backgroundColorByHexString('#efecec');
+      // set the color of icons and text of status bar to dark
       this.statusBar.styleDefault();
       this.splashScreen.hide();
       this.platform.registerBackButtonAction(() => {
