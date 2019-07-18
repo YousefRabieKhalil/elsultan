ecd0f49e75016561f499c138bceda5cc3ba34f69
Mohamed Hamed <mohamed.hamed@cis.asu.edu.eg>
2019-07-18 16:15:55 +0200

    Accept Some changes

--- src/providers/product-conntroller/product-conntroller.ts
+++ src/providers/product-conntroller/product-conntroller.ts
@@ -2,12 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SharedClass } from '../SharedClass';
 
-/*
-  Generated class for the ProductConntrollerProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class ProductConntrollerProvider {
   CartAdded = [] as any;
